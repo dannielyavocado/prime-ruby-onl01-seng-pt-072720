41355b0da2d4681d103b7d88aae6eebdf32666ab
Danniely Santos <dannielyingrid@gmail.com>
2020-08-08 21:48:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
+def prime?(integer)
+  if integer < 2 
+    return false 
+  end
+  
+  number = 2 
+  
+  while number < integer
+  if integer % number == 0 
+    return false 
+   end
+   number += 1 
+  end
+  return true 
+end
+  
+
+
 # Add  code here!
